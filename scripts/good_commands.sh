99e8e78cc00a09c2a8b67caba8d7f968de3ea309
alexeyche <alexey.chernushev@gmail.com>
2014-10-21 20:15:19 +0400

    cns

--- scripts/good_commands.sh
+++ scripts/good_commands.sh
@@ -3,14 +3,22 @@
 
 # analyse experiment results
 function show_reports {
+    path_to_show=./
+    if [ $# -gt 0 ]; then
+        path_to_show=
+        for var in "$@"; do
+            path_to_show="$path_to_show $var"
+        done
+    fi        
     len_of_report=
-    for i in $(find ./ -name "report_epoch*" ); do 
+    tmp_d=$(mktemp -d)
+    for i in $(find $path_to_show -name "report_epoch*"); do 
         if [ -z $len_of_report ]; then
             len_of_report=$(cat $i | wc -l)
         fi
-        tmp_d=$(mktemp -d )
-        tail -n+2 ./$i > $tmp_d/tail_file
-        yes "$i" | head -n $(($len_of_report - 1)) > $tmp_d/yes_file
-        paste $tmp_d/tail_file $tmp_d/yes_file 
+        bn=$(basename $i)
+        tail -n+2 ./$i > $tmp_d/tail_file_$bn
+        yes "$i" | head -n $(($len_of_report - 1)) > $tmp_d/yes_file_$bn
+        paste $tmp_d/tail_file_$bn $tmp_d/yes_file_$bn
     done | sort -r -k 4 | head -n 20
 }
